f092cd482101ad71b75abf2fc401e027141d2857
Sam Johnson <sam@durosoft.com>
2025-10-09 02:22:15 -0400

    clean up

--- benches/operations.rs
+++ benches/operations.rs
@@ -147,54 +147,5 @@ fn bench_remove(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_clear(c: &mut Criterion) {
-    let keys: Vec<i32> = (0..INPUT_SIZE as i32).collect();
-    let mut group = c.benchmark_group("clear");
-    group.throughput(Throughput::Elements(INPUT_SIZE as u64));
-
-    group.bench_function("pomap", |b| {
-        b.iter_batched(
-            || {
-                let mut map = PoMap::with_capacity(INPUT_SIZE);
-                for key in &keys {
-                    map.insert(*key, *key);
-                }
-                map
-            },
-            |mut map| {
-                map.clear();
-                black_box(map.len());
-            },
-            BatchSize::SmallInput,
-        );
-    });
-
-    group.bench_function("std_hashmap", |b| {
-        b.iter_batched(
-            || {
-                let mut map = HashMap::with_capacity(INPUT_SIZE);
-                for key in &keys {
-                    map.insert(*key, *key);
-                }
-                map
-            },
-            |mut map| {
-                map.clear();
-                black_box(map.len());
-            },
-            BatchSize::SmallInput,
-        );
-    });
-
-    group.finish();
-}
-
-criterion_group!(
-    benches,
-    bench_insert,
-    bench_get,
-    bench_update,
-    bench_remove,
-    bench_clear
-);
+criterion_group!(benches, bench_insert, bench_get, bench_update, bench_remove);
 criterion_main!(benches);
