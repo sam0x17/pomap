14f79dce26f8bf27cc8c37f6f2ee2827a233b908
Sam Johnson <sam@durosoft.com>
2026-01-06 03:46:17 -0500

    smaller

--- benches/pomap_bench.rs
+++ benches/pomap_bench.rs
@@ -40,8 +40,8 @@ const INSERT_INPUT_SIZE: usize = 50_000_usize;
 #[cfg(feature = "bench-string")]
 const MAX_INPUT_SIZE: usize = 250_000_usize;
 #[cfg(not(feature = "bench-string"))]
-const MAX_INPUT_SIZE: usize = 10_000_000_usize;
-const GETS_PER_ROUND: usize = 1_000_usize;
+const MAX_INPUT_SIZE: usize = 1_000_000_usize;
+const GETS_PER_ROUND: usize = 5_000_usize;
 const NUM_INTERMEDIATE_ROUNDS: usize = 10_usize;
 const HOT_SET: usize = MAX_INPUT_SIZE.isqrt();
 
